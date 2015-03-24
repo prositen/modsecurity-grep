6d51e4171fec1a2857050ba2b5e415237cf1fb4c
Anna Holmgren <anna.holmgren@wigetmedia.com>
2015-03-24 15:46:30 +0100

    Refactoring for PEP-8 compliance

--- greplog.py
+++ greplog.py
@@ -10,8 +10,14 @@ import json
 import re
 import subprocess
 import sys
-from termcolor import colored
 import urlparse
+
+
+try:
+    from termcolor import colored
+except ImportError:
+    def colored(text, args, **kwargs):
+        return text
 
 
 # Or I could rewrite the script for Python 3 which has enums
@@ -29,6 +35,7 @@ LogParts = enum('STARTED',
 
 Methods = enum('GET', 'POST', 'PUT')
 
+
 class Part(object):
     """ Base class for mod_security log parts """
     def __init__(self):
@@ -41,12 +48,12 @@ class Part(object):
             self.line_count =  line_count
         self.content.append(line)
 
-    def addParameter(self, line, line_count):
+    def add_parameter(self, line, line_count):
         parameters = ['%s=%s' % (key,item[0]) for key,item in urlparse.parse_qs(line).iteritems()]
         if parameters:
             self.content.extend(parameters)
 
-    def addJson(self, line, line_count):
+    def add_json(self, line, line_count):
         parameters = json.loads(line)
         for k,v in parameters.iteritems():
             self.content.append('%s=%s' % (k, json.dumps(v)))
@@ -66,10 +73,18 @@ class Part(object):
     def __iter__(self):
         return self.content.__iter__()
 
+
 class RequestHeaders(Part):
-    data = None
+    """ Contains all request headers.
+
+     Query parameters are parsed and accessible through get_parameters.
+    """
     QS = re.compile("(GET|POST|PUT) ([^\s\?]+)(\??(\S*)) .*")
-    request_url = ""
+
+    def __init__(self):
+        Part.__init__(self)
+        self._method = None
+        self.request_url = ""
 
     def add(self, line, line_count):
         Part.add(self, line, line_count)
@@ -84,31 +99,31 @@ class RequestHeaders(Part):
             self.parameters = [ '%s=%s' % (key,item[0]) for key,item in urlparse.parse_qs(result.group(4)).iteritems()]
             self.request_url = '%s %s\n' % (result.group(1), result.group(2))
 
-    def show(self, regex):
-        return ""
-
     def __str__(self):
         return str(self.request_url)
 
     def method(self):
         return self._method
 
+
 class Content(Part):
     def add(self, line, line_count):
         Part.add(self, line, line_count)
         try:
-            self.addJson(line, line_count)
+            self.add_json(line, line_count)
         except ValueError:
-            self.addParameter(line, line_count)
+            self.add_parameter(line, line_count)
 
 
 class Ignore(Part):
     def add(self, line, line_count):
         pass
 
+
 class Message():
     def __init__(self, line_count, args):
         self.line_count = line_count
+        self.args = args
         self.parts = dict()
         self.parts[LogParts.STARTED] = Ignore()
         self.parts[LogParts.REQUEST_HEADERS] = RequestHeaders()
@@ -117,16 +132,8 @@ class Message():
         self.parts[LogParts.STOPPED] = Ignore()
         self.parts[None] = Ignore()
 
-        self.include_headers = args.with_headers
-        self.exclude_headers = args.without_headers
-        self.include_parameters = args.with_parameters
-        self.exclude_parameters = args.without_parameters
-        self.show_headers = args.show_headers
-        self.lineno = args.n
-
     def method(self):
         return self.parts[LogParts.REQUEST_HEADERS].method()
-
 
     def add(self, state, line, line_count):
         if line:
@@ -140,33 +147,44 @@ class Message():
             str(self.parts[LogParts.CONTENT])))
 
     def show(self):
-        if self.include_headers:
-            if not any([self.parts[LogParts.REQUEST_HEADERS].matches(header) for header in self.include_headers]):
+        if self.args.with_headers:
+            if not any([self.parts[LogParts.REQUEST_HEADERS].matches(header) for header in self.args.with_headers]):
                 return False
-        if self.exclude_headers:
-            if any([self.parts[LogParts.REQUEST_HEADERS].matches(header) for header in self.exclude_headers]):
+        if self.args.without_headers:
+            if any([self.parts[LogParts.REQUEST_HEADERS].matches(header) for header in self.args.without_headers]):
                 return False
 
-        if self.include_parameters:
-            if not any([self.parts[LogParts.CONTENT].matches(param) for param in self.include_parameters]):
+        if self.args.with_parameters:
+            if not any([self.parts[LogParts.CONTENT].matches(param) for param in self.args.with_parameters]):
                 return False
-        if self.exclude_parameters:
-            if any([self.parts[LogParts.CONTENT].matches(param) for param in self.exclude_parameters]):
+        if self.args.without_parameters:
+            if any([self.parts[LogParts.CONTENT].matches(param) for param in self.args.without_parameters]):
                 return False
 
         return True
+
+    def request_headers(self):
+        return ("%s%s" % (
+                colored("%d: " % self.line_count, 'yellow', attrs=['bold']) if self.args.n else "",
+                self.parts[LogParts.REQUEST_HEADERS])
+            ).strip()
+
+    def content(self):
+        for x in iter(self.parts[LogParts.CONTENT]):
+            yield x.strip()
+
+    @staticmethod
+    def footer():
+        return '---'
 
     def handle(self):
         # Add query parameters to content
         self.parts[LogParts.CONTENT].extend(self.parts[LogParts.REQUEST_HEADERS].parameters)
         if self.show():
-            yield ("%s%s" % (
-                colored("%d: " % self.line_count, 'yellow', attrs=['bold']) if self.lineno else "",
-                self.parts[LogParts.REQUEST_HEADERS])
-            ).strip()
-            for x in iter(self.parts[LogParts.CONTENT]):
-                yield x.strip()
-            yield "---"
+            yield self.request_headers()
+            for x in self.content():
+                yield x
+            yield self.footer()
 
 
 class GrepLog():
@@ -177,7 +195,7 @@ class GrepLog():
         self.state = None
         self.message = Message(0, args)
 
-    def parseState(self, result, line_count):
+    def parse_state(self, result, line_count):
         logPart = result.group(2)
         if logPart == 'A':
             self.message = Message(line_count, self.args)
@@ -191,12 +209,12 @@ class GrepLog():
         else:
             self.state = LogParts.IGNORE
 
-    def parseLine(self, line, line_count):
+    def parse_line(self, line, line_count):
         """ Parse log line, handle depending on current state """
         result = self.DELIMITER_PATTERN.match(line)
 
         if result:
-            self.parseState(result, line_count)
+            self.parse_state(result, line_count)
         elif line:
             self.message.add(self.state, line, line_count)
 
@@ -215,49 +233,49 @@ def header(filename):
 def main(args):
     parser = argparse.ArgumentParser(description = 'Parse mod_security logs')
     parser.add_argument('--with_headers',
-        help='Show only logs where request headers match HEADER',
-        metavar='HEADER',
-        nargs='+')
+                        help='Show only logs where request headers match HEADER',
+                        metavar='HEADER',
+                        nargs='+')
     parser.add_argument('--without_headers',
-        help='Don\'t show logs where request headers match HEADER. Overrides --with_headers on conflicts',
-        metavar='HEADER',
-        nargs='+')
+                        help='Don\'t show logs where request headers match HEADER. '
+                             + 'Overrides --with_headers on conflicts',
+                        metavar='HEADER',
+                        nargs='+')
     parser.add_argument('--with_parameters',
-        help='Show only logs where URL parameters (GET, POST or PUT) match PARAM',
-        metavar='PARAM',
-        nargs='+')
+                        help='Show only logs where URL parameters (GET, POST or PUT) match PARAM',
+                        metavar='PARAM',
+                        nargs='+')
     parser.add_argument('--without_parameters',
-        help='Don\'t show logs where URL parameters match PARAM. Overrides --with_parameters on conflicts',
-        metavar='PARAM',
-        nargs='+')
+                        help='Don\'t show logs where URL parameters match PARAM. '
+                             + 'Overrides --with_parameters on conflicts',
+                        metavar='PARAM',
+                        nargs='+')
     parser.add_argument('--show_headers',
-        help='Display request headers SHOW_HEADERS. Normally only the GET/POST/PUT string is displayed',
-        nargs='+')
+                        help='Display request headers SHOW_HEADERS. Normally only the GET/POST/PUT string is displayed',
+                        nargs='+')
     parser.add_argument('-n',
-        help='Display line number',
-        action='store_true')
+                        help='Display line number',
+                        action='store_true')
     parser.add_argument('file', nargs='+')
 
     args = parser.parse_args(args)
 
     greplog = GrepLog(args)
     p = subprocess.Popen(['less', '-F', '-R', '-S', '-X', '-K'],
-                          stdin=subprocess.PIPE,
-                          stdout=sys.stdout)
+                         stdin=subprocess.PIPE,
+                         stdout=sys.stdout)
     filename = None
     try:
         for line in fileinput.input(args.file):
             if filename != fileinput.filename():
                 filename = fileinput.filename()
                 p.stdin.write(header(filename))
-            for x in greplog.parseLine(line, fileinput.filelineno()):
+            for x in greplog.parse_line(line, fileinput.filelineno()):
                 p.stdin.write("%s\n" % x)
         p.stdin.close()
         p.wait()
     except (KeyboardInterrupt, IOError):
         pass
-
-    #pager.page(parse(args))
 
 
 if __name__ == '__main__':
